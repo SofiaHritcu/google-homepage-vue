1e82effe2921db3c9b3ed09d44ee4144f38a8f21
SofiaHritcu <sofia.hritcu@principal33.com>
2021-08-16 21:02:22 +0300

    #Google - update: split footer into 2 vue components

---
+++ src/components/footer/FooterCountry.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="footer-country">
+        <div class="left-section">
+            <a class="footer-link" aria-disabled="true">România</a>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterCountry'
+}
+</script>
