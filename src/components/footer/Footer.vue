6aefb0fdf87bd0f75b0ed2d0a72df7525e248730
SofiaHritcu <sofia.hritcu@principal33.com>
2021-08-16 20:52:46 +0300

    #Google - add: footer content in only one vue component

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,9 +1,28 @@
 <template>
-    here is footer
+    <footer class="footer-links">
+            <div class="left-section">
+                <a class="footer-link" href="https://about.google/?utm_source=google-RO&utm_medium=referral&utm_campaign=hp-footer&fg=1">Despre</a>
+                <a class="footer-link" href="https://ads.google.com/intl/ro_ro/home/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1">Publicitate</a>
+                <a class="footer-link" href="https://www.google.com/services/?subid=ww-ww-et-g-awa-a-g_hpbfoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpbfooter&fg=1#?modal_active=none">Companii</a>
+                <a class="footer-link" href="https://www.google.com/search/howsearchworks/?fg=1">Cum funcționează Căutarea</a>
+              </div>
+              <div class="right-section">
+                <a class="footer-link" href="https://policies.google.com/privacy?hl=ro&fg=1">Confidențialitate</a>
+                <a class="footer-link" href="https://policies.google.com/terms?hl=ro&fg=1">Termeni</a>
+                <a class="footer-link" href="#">Setări</a>
+              </div>
+        </footer>
+        <footer class="footer-country">
+            <div class="left-section">
+                <a class="footer-link" aria-disabled="true">România</a>
+            </div>
+        </footer>
 </template>
 
 <script>
-export default {
-    name: 'Footer'
-}
+    export default {
+        name: 'Footer'
+    }
 </script>
+
+<style src="../../styles/footer.css"></style>
