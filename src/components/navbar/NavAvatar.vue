4771f91f852ad6740cb98484f796e5e771cda840
SofiaHritcu <sofia.hritcu@principal33.com>
2021-08-18 10:33:06 +0300

    #Google - add: avatar popup with hardcoded image

--- src/components/navbar/NavAvatar.vue
+++ src/components/navbar/NavAvatar.vue
@@ -1,11 +1,29 @@
 <template>
-    <a class="nav-link-round" target="_blank">
+    <a class="nav-link-round" target="_blank" v-on:click="showModal">
             <img class="user-avatar" src="../../assets/avatar-3.png" alt="" />
     </a>
+    <div id="myModal" class="modal">
+        <div class="modal-content">
+            <img src="../../assets/avatar-content.png" class="modal-img">
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
-        name: 'NavAvatar'
+    window.onclick = function(event) {
+        if (event.target == document.getElementById("myModal")) {
+            document.getElementById("myModal").style.display = "none";
+        }
     }
+
+    export default {
+        name: 'NavAvatar',
+        methods: {
+            showModal: function (){
+                var modal = document.getElementById("myModal");
+                modal.style.display = "block";
+            }
+        }
+    }
+    
 </script>
